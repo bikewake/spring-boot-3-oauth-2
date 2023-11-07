1988608c1d2c58a4acc34fe31e8251c87565fa5f
Lubo Sladok <bikewake@gmail.com>
2023-11-07 18:00:36 +0100

    add jquery solution, removed htmx dependency

---
+++ src/main/resources/static/app.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+
+    $('#chatForm').submit(function (e) {
+        e.preventDefault();
+        const message = $('#message').val();
+        $.post('/chat', { message});
+        $('#message').val('');
+    });
+
+    const eventSource = new EventSource('/chat');
+    eventSource.onmessage = function (event) {
+        if (event.data) {
+            const data = JSON.parse(event.data);
+            const dateString = new Date(data.timeStamp).toString();
+            const message = `<li class="message"><span class="sender">${data.sender}:</span>${data.message}<small class="timestamp">${dateString}</small></li>`;
+            $('#chatMessages').append(message + '');
+        }
+    };
+
+    eventSource.onerror = function (error) {
+        console.error('Chyba SSE:', error);
+        eventSource.close();
+    };
+
+
+});
